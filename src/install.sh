4a5a17da8beb06c0a651460e43e65cb8c9a72cbe
BurningPho3nix <pr@burningpho3nix.xyz>
2023-11-25 16:30:29 +0100

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -1,1 +1,30 @@
 #! /bin/sh
+
+sudo apt update && sudo apt upgrade -y
+
+sudo add-apt-repository universe -y && sudo add-apt-repository multiverse -y && sudo add-apt-repository restricted -y
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt install curl -y
+
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update
+
+sudo apt install ros-noetic-desktop-full -y
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
+
+sudo apt install python3-rosdep -y
+
+sudo rosdep init
+
+rosdep update
+
+sudo apt update && sudo apt upgrade -y
+
+sudo reboot
